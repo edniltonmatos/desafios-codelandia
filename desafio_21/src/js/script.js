3a9625ee0b2b96d494e7b433e21db3006b374ec5
ednilton dos santos matos <edniltonprogramador@gmail.com>
2022-04-29 09:37:09 -0300

    :bricks: desafio 02: estrutura de pastas/arquivos

---
+++ desafio_21/src/js/script.js
@@ -0,0 +1,22 @@
+//LOADING
+document.body.insertAdjacentHTML('beforeend', `
+  <div id="loading">
+    <svg class="spinner" viewBox="0 0 50 50">
+      <circle class="path" cx="25" cy="25" r="20" fill="none" stroke-width="3"></circle>
+    </svg>
+  </div>
+`)
+window.addEventListener('load', () => {
+  setTimeout(() => {
+    loading.remove()
+    app.removeAttribute('style')
+  }, 300);
+
+})
+//animação coração
+const card = document.querySelector("article")
+const coracao = document.querySelector("#coracao")
+
+card.addEventListener('click',function(){
+ coracao.style.color = "red"
+})
